793841647830dde69dc494b39ca872836482d346
loomhigh <loomhigh223555@gmail.com>
2026-05-01 14:38:19 +0800

    idk

--- AT1_KnowledgeTest/Test.py
+++ AT1_KnowledgeTest/Test.py
@@ -15,38 +15,3 @@ user_guess = 0
 attempts = 0
 
 print(secret_number)
-
-nix repl -f '<nixpkgs>'
-Nix 2.34.0
-Type :? for help.
-Loading installable ''...
-Added 26495 variables.
-AAAAAASomeThingsFailToEvaluate, AMB-plugins, ArchiSteamFarm, AusweisApp2, CHOWTapeModel, ChowCentaur, ChowKick, ChowPhaser, CoinMP, CuboCore, DisnixWebService, EBTKS, EmptyEpsilon, FIL-plugins, Fabric, HentaiAtHome, LAStools, LASzip, LASzip2, LPCNet
-... and 26475 more; view with :ll
-nix-repl> :b   (python3.withPackages (python-pkgs: with python-pkgs; [
-      dateutils
-      icalendar
-      ical
-      markdown
-      soupsieve #BeautifulSoup, converts html to txt
-      beautifulsoup4
-        >       dateutils
-        >       icalendar
-        >       ical
-        >       markdown
-        >       soupsieve #BeautifulSoup, converts html to txt
-        >       beautifulsoup4
-        >   ]))
-
-This derivation produced the following outputs:
-  out -> /nix/store/mx307b575sj0w7cy95d1f3a2fwqgnbvs-python3-3.13.12-env
-
-
-❯ /nix/store/mx307b575sj0w7cy95d1f3a2fwqgnbvs-python3-3.13.12-env/bin/python
-Python 3.13.12 (main, Feb  3 2026, 17:53:27) [Clang 21.1.8 ] on darwin
-Type "help", "copyright", "credits" or "license" for more information.
->>> import markdown
->>> markdown.
-markdown.Extension(         markdown.blockparser        markdown.extensions         markdown.markdown(          markdown.preprocessors      markdown.util
-markdown.Markdown(          markdown.blockprocessors    markdown.htmlparser         markdown.markdownFromFile(  markdown.serializers
-markdown.annotations        markdown.core               markdown.inlinepatterns     markdown.postprocessors     markdown.treeprocessors
